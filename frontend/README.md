edcae109106b1dc8c9a8fd158320913fe09038bd
Evgeny Arbatov <arbatovevgeny@gmail.com>
2024-05-06 18:29:28 +0800

    Trying to make Node to gRPC server

--- frontend/README.md
+++ frontend/README.md
@@ -3,3 +3,9 @@ Run
 ```
 npm run start
 ```
+
+Launch gRPC server locally:
+
+```
+docker run -p 9090:9090 evgenyarbatov/gpx-backend:latest
+```
--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const winston = require('winston');
 
-const port = 8080;
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
 
-const app = express();
+const port = process.env.SERVER_PORT || 8080;
+const gRPCServerURL = process.env.GRPC_SERVER || 'localhost:9090'
+
+const packageDefinition = protoLoader.loadSync(
+  './src/gpxtracker/gpxtracker.proto',
+  {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true
+  }
+);
+
+const gpxtracker_proto = grpc.loadPackageDefinition(packageDefinition).main;
+
+const gRPCClient = new gpxtracker_proto.GPXTracker(
+  gRPCServerURL,
+  grpc.credentials.createInsecure(),
+);
 
 const customFormat = winston.format.printf(({ level, message, timestamp }) => {
   return JSON.stringify({ timestamp, message  });
@@ -19,14 +39,49 @@ const logger = winston.createLogger({
   ]
 });
 
+const app = express();
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-app.get('/location', (req, res) => {
-  const { lat, lon } = req.query;
-  logger.info(`Location: lat=${lat}, lng=${lon}`);
-  res.json({ lat: lat, lng: lon });
+app.get('/location', async (req, res) => {
+  const { lat, lng } = req.query;
+  logger.info(`Location: lat=${lat}, lng=${lng}`);
+
+  const request = {
+    lat: lat,
+    lng: lng,
+  };
+
+  const requests = [
+    new Promise((resolve, reject) => {
+      gRPCClient.getAddress(request, (err, response) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(response);
+        }
+      });
+    }),
+    new Promise((resolve, reject) => {
+      gRPCClient.getRemainginDistance(request, (err, response) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(response);
+        }
+      });
+    }),
+  ];
+
+  Promise.all(requests)
+    .then(responses => {
+      res.json({ lat: lat, lng: lng, responses: responses });
+    })
+    .catch(error => {
+      res.json({ error: error });
+    });
 });
 
 app.listen(port, () => {
