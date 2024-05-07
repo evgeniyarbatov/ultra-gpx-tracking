a521319494b9058decfb854df15ebf070bd25772
Evgeny Arbatov <arbatovevgeny@gmail.com>
2024-05-07 16:00:36 +0800

    Add time estimate to postgis

--- postgis/init/01_seed.sql
+++ postgis/init/01_seed.sql
@@ -3,23 +3,25 @@ CREATE EXTENSION IF NOT EXISTS postgis;
 CREATE TABLE gpx_route (
     id SERIAL PRIMARY KEY,
     location GEOMETRY(Point, 4326),
-    distance DECIMAL
+    distance DECIMAL,
+    estimated_time VARCHAR(255)
 );
 
 CREATE TEMP TABLE tmp_route (
     id SERIAL PRIMARY KEY,
     latitude DECIMAL,
     longitude DECIMAL,
-    distance DECIMAL
+    distance DECIMAL,
+    estimated_time VARCHAR(255)
 );
 
-COPY tmp_route(latitude, longitude, distance)
+COPY tmp_route(latitude, longitude, distance, estimated_time)
 FROM '/data/gpx-route.csv'
 DELIMITER ','
 CSV HEADER;
 
-INSERT INTO gpx_route (location, distance)
-SELECT ST_SetSRID(ST_MakePoint(longitude, latitude), 4326), distance
+INSERT INTO gpx_route (location, distance, estimated_time)
+SELECT ST_SetSRID(ST_MakePoint(longitude, latitude), 4326), distance, estimated_time
 FROM tmp_route;
 
 DROP TABLE tmp_route;
